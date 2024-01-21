13a5d6fc4d0e451091a7628974b36196722e8134
Royanrp045 <royanristuprayoga@gmail.com>
2024-01-21 19:09:39 +0700

    first commit

--- Landmarks/Views/LandmarksApp.swift
+++ Landmarks/Views/LandmarksApp.swift
@@ -1,17 +1,15 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by Royan Ristu Prayoga on 03/01/24.
-//
-
 import SwiftUI
+
 
 @main
 struct LandmarksApp: App {
+    @State private var modelData = ModelData()
+
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(modelData)
         }
     }
 }
