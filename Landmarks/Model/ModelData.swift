870f0bdff1114ccc24fe550bf8969b8bca7e2086
Royanrp045 <royanristuprayoga@gmail.com>
2024-02-07 11:53:20 +0700

    Create a category list

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -5,6 +5,13 @@ import Foundation
 class ModelData {
     var landmarks: [Landmark] = load("landmarkData.json")
     var hikes: [Hike] = load("hikeData.json")
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(
+            grouping: landmarks,
+            by: { $0.category.rawValue }
+        )
+    }
 }
 
 
