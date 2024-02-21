ec3eb33cd0345b63fea0e361036c8393720ff630
Royanrp045 <royanristuprayoga@gmail.com>
2024-02-21 09:57:26 +0700

    Create a view to represent a UIPageViewController

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -23,6 +23,9 @@ struct Landmark: Hashable, Codable, Identifiable {
     var image: Image {
         Image(imageName)
     }
+    var featureImage: Image? {
+        isFeatured ? Image(imageName + "_feature") : nil
+    }
 
 
     private var coordinates: Coordinates
